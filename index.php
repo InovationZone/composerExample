a68c1140479b4010225dffeee616dc867e82a4ec
João Paulo <joaopaulosil@yahoo.com.br>
2018-10-02 23:51:25 -0300

    composer example

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+
+$is_valid = GUMP::is_valid($_POST, array(
+    'username' => 'required|alpha_numeric',
+    'password' => 'required|max_len,100|min_len,6'
+));
+
+if($is_valid === true) {
+    // continue
+} else {
+    print_r($is_valid);
+}
+
